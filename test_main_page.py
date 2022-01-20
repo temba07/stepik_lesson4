89815ae6fc93af041bfb5fb0d4dbc628ab37deb8
Tembulat_Ketov <tketov@list.ru>
2022-01-20 17:36:30 +0300

    Редактирование test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,8 @@
-# def test_guest_can_go_to_login_page(browser):
-    # browser.get(link)
-    # login_link = browser.find_element_by_css_selector("#login_link")
-    # login_link.click()
-
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-   browser.get(link)
-   go_to_login_page(browser)
-
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
