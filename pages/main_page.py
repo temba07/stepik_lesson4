68262c17903b97e135cd462c262dbdd5476aba9f
Tembulat_Ketov <tketov@list.ru>
2022-01-23 18:28:42 +0300

    no message

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,8 +6,10 @@ from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link = self.browser.find_element_by_css_selector("#login_link")
         link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
 
     def should_be_login_link(self):
         assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
